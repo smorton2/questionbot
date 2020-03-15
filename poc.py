d99c49d7da475709fe536d15dcd9dfbb4f3503b2
Stephen Morton <24463917+smorton2@users.noreply.github.com>
2020-03-15 15:52:17 -0500

    initialize repository with proof of concept bot

---
+++ poc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Imports
+import csv
+import random
+import requests
+import os
+
+# Find a random row and return a formatted version of it.
+csv_read = open('questionbank.csv')
+csv_reader = csv.reader(csv_read, delimiter=',')
+row = random.choice(list(csv_reader)[1:])
+
+# ToDo: Store URL somewhere safe.
+URL = os.environ.get('SLACK_TOKEN')
+
+# Message
+message = {"text": f'{row[1]}'}
+requests.post(url=URL, json=message)
+
+# ToDo: Some logic to make sure each user only sees a given question once.
